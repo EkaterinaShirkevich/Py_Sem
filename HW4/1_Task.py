3e13e6526036edf125438461ef42a7b737cd19f4
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-07 16:52:47 +0300

    HW4_required_accuracy

--- HW4/1_Task.py
+++ HW4/1_Task.py
@@ -5,3 +5,9 @@
 # Enter the required accuracy '0.0001': 0.000001
 # out
 # 9.000000
+from decimal import Decimal
+
+
+a = Decimal(input("Enter a real number: "))
+b = Decimal(input("Enter the required accuracy 0.0001: "))
+print(a.quantize(Decimal(b)))
