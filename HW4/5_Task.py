1a46359c0e86874cc4678b095d0b970e164d044f
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-08 20:47:05 +0300

    poly_sum_to_file

--- HW4/5_Task.py
+++ HW4/5_Task.py
@@ -13,7 +13,7 @@ from random import choice
 
 def poly_sum(name_1: str, name_2: str):
     with open(name_1, "r", encoding="utf-8") as my_f_1, \
-            open(name_2, "r", encoding="utf-8") as my_f_2:
+            open(name_2, "r", encoding="utf-8") as my_f_2: # для длинных выражений переход на новую строку через \
         first = my_f_1.readlines()
         second = my_f_2.readlines()
 
