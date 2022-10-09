ffdc936ab92c4bbe7c9f5aae3dba2111f9aef6c6
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-09 11:38:50 +0300

    calc_arithm_from_string

--- HW5/2_Task.py
+++ HW5/2_Task.py
@@ -4,3 +4,17 @@
 # exists
 #from itertools import groupby, starmap
 
+#Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+from itertools import groupby, starmap
+from os import path
+
+
+def rle_encode(text="text_words.txt", code_text="text_code_words.txt"):
+    if path.exists(text) and not path.exists(code_text):
+        with open(text) as my_f_1, \
+                open(code_text, "a") as my_f_2:
+            for i in my_f_1:
+                my_f_2.write("".join([f"{len(list(v))}{ch}" for ch, v in groupby(i)]))
+    else:
+        print("The files do not exist in the system!")
