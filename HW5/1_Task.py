af57f3db7ff6628896bee678c6dd3bf38d586c10
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-09 18:34:33 +0300

    del_from_str_set

--- HW5/1_Task.py
+++ HW5/1_Task.py
@@ -1,3 +1,25 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 # В тексте используется разделитель пробел.
 
+from os import remove
+from random import sample
+
+
+def form_words(count: int, source_set: str):
+    words = []
+    for i in range(count):       
+        new_set = sample(source_set, len(source_set))
+        words.append(''.join(new_set))
+    return ' '.join(words)   
+
+def del_set(words: str) -> str:
+    to_del = input("Введите комбинацию, которую нужно удалить: ")
+    return words.replace(to_del, "")
+
+count = int(input("Введите количество желаемых комбинаций: "))
+set = input("Введите буквы, из которых будут составлены комбинации: ")
+words = form_words(count, set)
+print(words)
+print(del_set(words))
+    
+
