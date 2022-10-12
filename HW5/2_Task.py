9f56f53f789d63ddbd2b5aee385f07cbde9cb07d
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-12 14:17:23 +0300

    rle

--- HW5/2_Task.py
+++ HW5/2_Task.py
@@ -15,6 +15,22 @@ def rle_encode(text="text_words.txt", code_text="text_code_words.txt"):
         with open(text) as my_f_1, \
                 open(code_text, "a") as my_f_2:
             for i in my_f_1:
-                my_f_2.write("".join([f"{len(list(v))}{ch}" for ch, v in groupby(i)]))
+                my_f_2.write("".join([f"{len(list(count))}{letter}" for letter, count in groupby(i)]))
     else:
         print("The files do not exist in the system!")
+
+
+def rle_decode(name):
+    if path.exists(name):
+        with open(name) as my_f:
+            word_num = []
+            for i in my_f:
+                word_num = [''.join(g) for k, g in groupby(i.strip(), key=str.isdigit)]
+                print(''.join([f"{int(word_num[i]) * word_num[i + 1]}" for i in range(0, len(word_num), 2)]))
+    else:
+        print("The files do not exist in the system!")
+
+
+
+rle_encode(input("Enter the name of the file with the text: "), input("Enter the file name to record: "))
+rle_decode(input("Enter the name of the file to decode: "))                            
